ad4bc1877f17b031d0bd2da85c1ebcbeb69552e2
aapanel.com <fan@aapanel.com>
2025-07-05 15:16:52 +0800

    rspamd-test

--- Dockerfiles/rspamd/rspamd.sh
+++ Dockerfiles/rspamd/rspamd.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 
+if [ ! -f "/etc/rspamd/rspamd.conf" ]; then
+  cp -rnp /rspamd_init/* /etc/rspamd/
+fi
+
+if [ ! -f "/etc/rspamd/local.d/milter_headers.conf" ]; then
+  cp -rnp /rspamd_init/* /etc/rspamd/
+fi
+
+# Check if there is an bind_socket = "*:11334"; configuration, and if not, force copy the file
+CHECK_CONF=$(grep 'bind_socket = "*:11334";' /etc/rspamd/rspamd.conf)
+if [ -z "${CHECK_CONF}" ]; then
+  \cp -arpf /rspamd_init/rspamd.conf /etc/rspamd/rspamd.conf
+fi
+
+# Check if there is an services =  configuration, and if not, force copy the file
+CHECK_CONF=$(grep "services = " /etc/rspamd/statistic.conf)
+if [ -z "${CHECK_CONF}" ]; then
+  \cp -arpf /rspamd_init/statistic.conf /etc/rspamd/statistic.conf
+fi
+
+
 chmod 755 /var/lib/rspamd
 chown -R _rspamd:_rspamd /var/lib/rspamd
 
